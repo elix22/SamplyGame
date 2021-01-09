6f7e42b70f692f26df6fe0535aa6add66d0423fc
Eli Aloni <elix22@gmail.com>
2021-01-09 21:42:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Urho;
+
+//dotnet publish -r win-x64 -c release /p:PublishSingleFile=true /p:PublishTrimmed=true -p:PublishReadyToRun=true
+//osx-x64 , 
+namespace SamplyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SamplyGame().Run();
+        }
+    }
+}
+
